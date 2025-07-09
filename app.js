d656dc9f85cd747b94ba5150369f418f3b55c5bb
ajay26188 <ajaysah52@gmail.com>
2025-07-09 19:31:08 +0530

    /health endpoint added

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
