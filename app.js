9500a5aa10a1cd9d3a2d5a2fcdad2111399ccccf
ajay26188 <ajaysah52@gmail.com>
2025-07-09 20:04:36 +0530

    fix: restore working health check

--- app.js
+++ app.js
@@ -8,11 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
-
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
