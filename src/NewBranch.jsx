c6eb76af5808f7458b681222f1e0a292f7fd3d91
ajay26188 <ajaysah52@gmail.com>
2025-07-11 09:41:32 +0530

    NewBranch.jsx added

---
+++ src/NewBranch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewBranch = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'Arial' }}>
+      <h1>New Branch Demo</h1>
+      <p>This component was created on a new branch called new-branch!</p>
+    </div>
+  );
+};
+
+export default NewBranch;
